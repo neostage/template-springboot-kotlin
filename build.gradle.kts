0631606c1f1c877d98f3eada65a51b5803f9ae2e
neostage <jayowu21c@gmail.com>
2022-08-04 00:46:34 +0900

    updated all versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,29 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
 
-plugins {
-    id("org.springframework.boot") version "2.5.0"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.0"
-    kotlin("plugin.spring") version "1.5.0"
-    kotlin("plugin.jpa") version "1.5.0"
-    kotlin("plugin.serialization") version "1.5.0"
-    id("org.jmailen.kotlinter") version "3.4.4"
-    id("io.gitlab.arturbosch.detekt") version "1.17.1"
-}
+val querydslGeneratedDir = "$buildDir/generated"
+val querydslKtOutputSubDirFull = "$querydslGeneratedDir/source/kaptKotlin"
 
 group = "com.dasrida.template.springboot"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_16
+java.sourceCompatibility = JavaVersion.VERSION_17
 
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
-}
-
-sourceSets {
-    main {
-        kotlin {
-            
-        }
-    }
+plugins {
+    id("org.springframework.boot") version "2.7.2"
+    id("io.spring.dependency-management") version "1.0.12.RELEASE"
+    kotlin("jvm") version "1.7.10" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.jvm
+    kotlin("plugin.spring") version "1.7.10" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.plugin.spring
+    kotlin("plugin.jpa") version "1.7.10" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.plugin.jpa
+    kotlin("plugin.serialization") version "1.7.10" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.plugin.serialization
+    id("org.jmailen.kotlinter") version "3.11.1" // https://github.com/jeremymailen/kotlinter-gradle/releases
+    id("io.gitlab.arturbosch.detekt") version "1.21.0" // https://github.com/detekt/detekt/releases
+    id("org.jetbrains.dokka") version "1.7.10" // https://plugins.gradle.org/plugin/org.jetbrains.dokka
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.7.10" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.plugin.allopen
 }
 
 repositories {
     mavenCentral()
-    jcenter {
-        content {
-            includeModule("org.jetbrains.kotlinx", "kotlinx-html-jvm")
-        }
-    }
-    maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {
@@ -48,19 +35,27 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     // https://github.com/Kotlin/kotlinx.serialization -> Dependency on the JSON library 참조
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0-RC")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    // implementation("org.springframework.kafka:spring-kafka")
-    compileOnly("org.projectlombok:lombok")
-    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    // testImplementation("org.springframework.kafka:spring-kafka-test")
     testImplementation("org.springframework.security:spring-security-test")
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir(querydslKtOutputSubDirFull)
+        }
+    }
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
 }
 
 detekt {
@@ -68,30 +63,42 @@ detekt {
     allRules = false // activate all available (even unstable) rules.
     config = files("$projectDir/config/detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
     baseline = file("$projectDir/config/baseline.xml") // a way of suppressing issues before introducing detekt
+}
 
-    reports {
-        html.enabled = true // observe findings in your browser with structure and code snippets
-        xml.enabled = true // checkstyle like format mainly for integrations like Jenkins
-        txt.enabled = true // similar to the console output, contains issue signature to manually edit baseline files
-        sarif.enabled = true // standardized SARIF format (https://sarifweb.azurewebsites.net/) to support integrations with Github Code Scanning
+kotlinter {
+    ignoreFailures = false
+    reporters = arrayOf("checkstyle", "plain")
+    experimentalRules = false
+    disabledRules = emptyArray()
+}
+
+tasks {
+    withType<Detekt>().configureEach {
+        reports {
+            html.required.set(true) // observe findings in your browser with structure and code snippets
+            xml.required.set(true) // checkstyle like format mainly for integrations like Jenkins
+            txt.required.set(true) // similar to the console output, contains issue signature to manually edit baseline files
+            sarif.required.set(true) // standardized SARIF format (https://sarifweb.azurewebsites.net/) to support integrations with Github Code Scanning
+            md.required.set(true) // simple Markdown format
+        }
+        jvmTarget = "17"
     }
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "16"
+    withType<DetektCreateBaselineTask>().configureEach {
+        jvmTarget = "17"
     }
-}
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
 
-// Kotlin DSL
-tasks.withType<io.gitlab.arturbosch.detekt.Detekt> {
-    onlyIf { project.hasProperty("runDetekt") }
-    // JDK 16은 아직 공식 지원 중은 아님
-    // Target version of the generated JVM bytecode. It is used for type resolution.
-    jvmTarget = "16"
+    withType<Test> {
+        useJUnitPlatform()
+    }
+
+    withType<org.jmailen.gradle.kotlinter.tasks.LintTask> {
+        setSource(source - fileTree(querydslGeneratedDir))
+    }
 }
